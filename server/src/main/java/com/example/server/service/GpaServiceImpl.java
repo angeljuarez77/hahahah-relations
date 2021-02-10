5e2156c2b7ccb5dd170e9be862d95db679cc240d
Angel Juarez <angeljuarez729@gmail.com>
2021-02-10 12:36:28 -0500

    imported gpa repository bean to service

--- server/src/main/java/com/example/server/service/GpaServiceImpl.java
+++ server/src/main/java/com/example/server/service/GpaServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.server.service;
 
 import com.example.server.model.Gpa;
+import com.example.server.repository.GpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GpaServiceImpl implements GpaService {
+    @Autowired
+    GpaRepository gpaRepository;
+
     @Override
     public Iterable<Gpa> getGpas() {
         return null;
