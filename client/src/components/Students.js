e98bd0de4921964233954a66da5fab6b2b85e8c3
Angel Juarez <angeljuarez729@gmail.com>
2021-02-10 15:11:46 -0500

    created post functionality to create new student

--- client/src/components/Students.js
+++ client/src/components/Students.js
@@ -17,6 +17,26 @@ function Students() {
     getStudents();
   }, [])
 
+  const [form, setForm] = useState(null);
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    createStudent();
+  }
+
+  async function createStudent() {
+    try {
+      const res = await axios.post('http://localhost:8080/students', form);
+      console.log(res.data);
+    } catch(e) {
+      console.error(e, e.message);
+    }
+  }
+
   return(
     <div>
       { students && students.map(student => (
@@ -25,6 +45,35 @@ function Students() {
           <h6>They are currently in grade { student.grade }</h6>
         </div>
       ))}
+
+      <div>
+        <h2>Enroll a new student!</h2>
+        <form
+          onChange={ (e) => handleChange(e) }
+          onSubmit={ (e) => handleSubmit(e) }>
+          <label>
+            First Name:
+            <input type="text" name="firstName" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="lastName" />
+          </label>
+          <label>
+            Age:
+            <input type="text" name="age" />
+          </label>
+          <label>
+            Grade Level:
+            <input type="text" name="grade" />
+          </label>
+          <label>
+            School Name:
+            <input type="text" name="schoolName" />
+          </label>
+          <input type="submit" value="Enroll student" />
+        </form>
+      </div>
     </div>
   )
 }
