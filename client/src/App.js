28461edbc25d577573cb9b8fe903887977cb9685
Angel Juarez <angeljuarez729@gmail.com>
2021-02-10 14:34:29 -0500

    react, installed axios-react router. added components folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
