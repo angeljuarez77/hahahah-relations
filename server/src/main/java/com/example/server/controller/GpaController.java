d3ff5a7edc463fe68e3e12577beac9991638a6a6
Angel Juarez <angeljuarez729@gmail.com>
2021-02-10 12:42:57 -0500

    imported gpa service bean

--- server/src/main/java/com/example/server/controller/GpaController.java
+++ server/src/main/java/com/example/server/controller/GpaController.java
@@ -1,9 +1,13 @@
 package com.example.server.controller;
 
+import com.example.server.service.GpaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/gpas")
 public class GpaController {
+    @Autowired
+    GpaService gpaService;
 }
