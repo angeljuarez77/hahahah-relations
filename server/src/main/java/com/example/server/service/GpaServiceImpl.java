08d4a439cde8c1dd980f9c74384ed5a49376e158
Angel Juarez <angeljuarez729@gmail.com>
2021-02-11 00:16:43 -0500

    get student to set when gpa created (service layer only)

--- server/src/main/java/com/example/server/service/GpaServiceImpl.java
+++ server/src/main/java/com/example/server/service/GpaServiceImpl.java
@@ -1,13 +1,18 @@
 package com.example.server.service;
 
 import com.example.server.model.Gpa;
+import com.example.server.model.Student;
 import com.example.server.repository.GpaRepository;
+import com.example.server.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GpaServiceImpl implements GpaService {
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @Autowired
     GpaRepository gpaRepository;
@@ -18,8 +23,9 @@ public class GpaServiceImpl implements GpaService {
     }
 
     @Override
-    public Gpa createGpa(Gpa gpa) {
-        return gpaRepository.save(gpa);
+    public Gpa createGpa(Long id, Gpa gpa) {
+        Student student = studentRepository.findById(id).get();
+        return gpaRepository.save(gpa).setStudent(student);
     }
 
     @Override
